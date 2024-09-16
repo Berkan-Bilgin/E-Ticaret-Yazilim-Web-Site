4823615b890ee92525f0dea2c3d4f37749f8db5f
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2024-09-16 23:11:04 +0300

    add fontawesome core package

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,9 @@
 import localFont from "next/font/local";
 import "./globals.css";
+// import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+
+// config.autoAddCss = false;
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
