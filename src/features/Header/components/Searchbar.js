456ca25bc627199028fd89199b8030dd7e82860d
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2024-09-25 05:52:09 +0300

    change z index

--- src/features/Header/components/Searchbar.js
+++ src/features/Header/components/Searchbar.js
@@ -11,7 +11,7 @@ const Searchbar = () => {
   }, []);
 
   return (
-    <div className="bg-white sticky top-0 z-10">
+    <div className="bg-white sticky top-0 z-[19]">
       <div className="flex items-center sm:space-x-4 px-4 py-2 justify-between container">
         <Logo />
         <SearchInput />
